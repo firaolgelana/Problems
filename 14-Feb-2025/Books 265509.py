7f4ec343b8badf33b52ec0f05bc627239830a00b
firaolgelana <gelanafiraol@gmail.com>
2025-02-14 14:31:12 +0300

    Created 'Books' by A2SV Hub

---
+++ 14-Feb-2025/Books 265509.py
@@ -0,0 +1,14 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+n, t = map(int, input().split())
+books = list(map(int, input().split()))
+
+max_books = current_books = left = 0
+for right in range(n):
+    current_books += books[right]
+    while current_books > t and left <= right:
+        current_books -= books[left]
+        left += 1
+    max_books = max(max_books, right - left + 1)
+
+print(max_books)
