57e724a7e7e6bbbb3d1b5638ac94dd555806962f
firaolgelana <gelanafiraol@gmail.com>
2025-02-14 09:30:35 +0300

    Created '3Sum Closest ' by A2SV Hub

---
+++ 14-Feb-2025/3Sum Closest  264878.py
@@ -0,0 +1,23 @@
+# Problem: 3Sum Closest  - https://leetcode.com/problems/3sum-closest/
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        size = len(nums)
+        nums.sort()
+        print(nums)
+        closest = nums[0] + nums[1] + nums[2]
+        for i in range(size):
+            left, right = i + 1, size - 1
+            while left < right:
+                current_sum = nums[i] + nums[left] + nums[right] 
+                if abs(current_sum - target) < abs(closest - target):
+                    closest = current_sum
+                if current_sum < target:
+                    left += 1
+                else:
+                    right -= 1
+        
+        return closest
+
+
+        
