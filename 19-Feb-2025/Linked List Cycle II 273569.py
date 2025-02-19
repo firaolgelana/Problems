cc397dea42ae14589007a3712fa2b0a93a0c7e05
firaolgelana <gelanafiraol@gmail.com>
2025-02-19 15:10:55 +0300

    Created 'Linked List Cycle II' by A2SV Hub

---
+++ 19-Feb-2025/Linked List Cycle II 273569.py
@@ -0,0 +1,19 @@
+# Problem: Linked List Cycle II - https://leetcode.com/problems/linked-list-cycle-ii/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        seen = set()
+        while head:
+            if head in seen:
+                return head
+            seen.add(head)
+            head = head.next
+
+        return None
+        
