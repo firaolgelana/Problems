8a71e573355e9a701a681bb65dce6e06e90e1199
firaolgelana <gelanafiraol@gmail.com>
2025-02-26 21:55:37 +0300

    Created 'D - Minimize the Distance' by A2SV Hub

---
+++ A2SV G6 - Round #4 26-Feb-2025/D - Minimize the Distance 285361.py
@@ -0,0 +1,28 @@
+# Problem: D - Minimize the Distance - https://codeforces.com/gym/590053/problem/D
+
+from collections import defaultdict, deque, Counter
+import sys
+
+input = sys.stdin.readline
+
+def iin():return int(input())
+
+def sin():return input().strip()
+
+def lin():return list(map(int, input().split()))
+
+def stol():return list(input().strip)
+
+def minp(): return map(int, input().split())
+
+def main():
+    n = iin()
+    arr = lin()
+    arr.sort()
+    print(arr[(n-1)//2])
+    
+if __name__ == "__main__":
+    # t = int(input())
+    t = 1
+    for _ in range(t):
+        main()
