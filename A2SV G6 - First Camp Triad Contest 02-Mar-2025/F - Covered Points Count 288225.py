0dcfa0992fcd6b90ace3daa6138b2ce7fe21cc79
firaolgelana <gelanafiraol@gmail.com>
2025-03-02 19:54:01 +0300

    Created 'F - Covered Points Count' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 02-Mar-2025/F - Covered Points Count 288225.py
@@ -0,0 +1,38 @@
+# Problem: F - Covered Points Count - https://codeforces.com/gym/589822/problem/F
+
+from collections import defaultdict, deque, Counter
+from bisect import bisect_left, bisect_right
+import sys, math
+ 
+input = sys.stdin.readline
+ 
+def iin():return int(input())
+ 
+def sin():return input().strip()
+ 
+def lin():return list(map(int, input().split()))
+  
+def minp(): return map(int, input().split())
+
+def main():
+    n = iin()
+    segments = defaultdict(int)
+    for _ in range(n):
+        start, end = minp()
+        segments[start] += 1
+        segments[end + 1] -= 1
+
+    keys = sorted(segments.keys())
+    coverage = [0] * (n + 1)
+    pt = 0
+    for i in range(len(keys) - 1):
+        pt += segments[keys[i]]
+        coverage[pt] += keys[i+1] - keys[i]
+    
+    print(*coverage[1:])
+
+
+if __name__ == "__main__":
+    t = 1
+    for i in range(t):
+        main()
