12976db9c874f6b1f88046a6f543556cda23fdc3
firaolgelana <gelanafiraol@gmail.com>
2025-03-02 15:21:12 +0300

    Created 'D - Socialism' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 02-Mar-2025/D - Socialism 288098.py
@@ -0,0 +1,32 @@
+# Problem: D - Socialism - https://codeforces.com/gym/589822/problem/D
+
+from collections import defaultdict, deque, Counter
+from bisect import bisect_left, bisect_right
+import sys, math
+ 
+input = sys.stdin.readline
+ 
+def iin():return int(input())
+ 
+def sin():return input().strip()
+ 
+def lin():return list(map(int, input().split()))
+  
+def minp(): return map(int, input().split())
+
+def main():
+    n, x = minp()
+    arr = lin()
+    arr.sort(reverse=True)
+    _sum = 0
+    for i in range(n):
+        _sum += arr[i]
+        if _sum / (i + 1) < x:
+            print(i)
+            return
+    print(n)
+
+if __name__ == "__main__":
+    t = iin()
+    for i in range(t):
+        main()
