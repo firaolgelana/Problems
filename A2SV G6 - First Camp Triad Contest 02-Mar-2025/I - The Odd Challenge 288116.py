07756ca67698e1340743b43f3d775bbef6412ab7
firaolgelana <gelanafiraol@gmail.com>
2025-03-02 16:08:34 +0300

    Created 'I - The Odd Challenge' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 02-Mar-2025/I - The Odd Challenge 288116.py
@@ -0,0 +1,31 @@
+# Problem: I - The Odd Challenge - https://codeforces.com/gym/589822/problem/I
+
+from collections import defaultdict, deque, Counter
+from bisect import bisect_left, bisect_right
+import sys, math
+ 
+input = sys.stdin.readline
+ 
+def iin():return int(input())
+ 
+def sin():return input().strip()
+ 
+def lin():return list(map(int, input().split()))
+  
+def minp(): return map(int, input().split())
+
+def main():
+    n, m = minp()
+    arr = lin()
+    prefix = [0]
+    for num in arr:
+        prefix.append(prefix[-1] + num)
+    for _ in range(m):
+        l, r, k = minp()
+        _sum = prefix[-1] - (prefix[r] - prefix[l-1] + k * (r - l + 1))
+        print("YES" if _sum % 2 else "NO")
+
+if __name__ == "__main__":
+    t = iin()
+    for i in range(t):
+        main()
