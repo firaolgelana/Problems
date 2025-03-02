f89598c9bd9fe63f57e037ff7b1778cc5adb77a0
firaolgelana <gelanafiraol@gmail.com>
2025-03-02 15:05:59 +0300

    Created 'B - Kidus Couldn't Sleep' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 02-Mar-2025/B - Kidus Couldn't Sleep 288093.py
@@ -0,0 +1,33 @@
+# Problem: B - Kidus Couldn't Sleep - https://codeforces.com/gym/589822/problem/B
+
+from collections import defaultdict, deque, Counter
+from bisect import bisect_left, bisect_right
+import sys, math
+ 
+input = sys.stdin.readline
+ 
+def iin():return int(input())
+ 
+def sin():return input().strip()
+ 
+def lin():return list(map(int, input().split()))
+  
+def minp(): return map(int, input().split())
+
+def main():
+    n, k = minp()
+    arr = lin()
+    d = n - k + 1
+    _sum = sum(arr[:d])
+    total = _sum
+    for i in range(n - d):
+        _sum -= arr[i]
+        _sum += arr[i+d]
+        total += _sum
+    
+    print(total / d)
+
+if __name__ == "__main__":
+    t = 1
+    for i in range(t):
+        main()
