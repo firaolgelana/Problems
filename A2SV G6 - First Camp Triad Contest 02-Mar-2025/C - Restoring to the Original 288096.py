e80476971544bc4782646d94db0d2d3f0753d9b5
firaolgelana <gelanafiraol@gmail.com>
2025-03-02 15:13:01 +0300

    Created 'C - Restoring to the Original' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 02-Mar-2025/C - Restoring to the Original 288096.py
@@ -0,0 +1,28 @@
+# Problem: C - Restoring to the Original - https://codeforces.com/gym/589822/problem/C
+
+from collections import defaultdict, deque, Counter
+from bisect import bisect_left, bisect_right
+import sys, math
+ 
+input = sys.stdin.readline
+ 
+def iin():return int(input())
+ 
+def sin():return input().strip()
+ 
+def lin():return list(map(int, input().split()))
+  
+def minp(): return map(int, input().split())
+
+def main():
+    n = iin()
+    s = sin()
+    ones = s.count('n')
+    zeros = s.count('r')
+    print('1 ' * ones , end="")
+    print('0 ' *zeros)
+
+if __name__ == "__main__":
+    t = 1
+    for i in range(t):
+        main()
