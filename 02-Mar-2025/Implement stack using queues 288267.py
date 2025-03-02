bc9476575960ff7e37c6d058e715c87ddb63315b
firaolgelana <gelanafiraol@gmail.com>
2025-03-02 20:52:28 +0300

    Created 'Implement stack using queues' by A2SV Hub

---
+++ 02-Mar-2025/Implement stack using queues 288267.py
@@ -0,0 +1,32 @@
+# Problem: Implement stack using queues - https://leetcode.com/problems/implement-stack-using-queues/
+
+class MyStack:
+
+    def __init__(self):
+        self.queue = deque()
+
+    def push(self, x: int) -> None:
+        self.queue.appendleft(x)
+        
+    def pop(self) -> int:
+        self.queue.rotate(-1)
+        removed = self.queue.pop()
+        return removed
+
+    def top(self) -> int:
+        self.queue.rotate(-1)
+        top = self.queue.pop()
+        self.queue.appendleft(top)
+        return top
+
+    def empty(self) -> bool:
+        return not self.queue
+        
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
