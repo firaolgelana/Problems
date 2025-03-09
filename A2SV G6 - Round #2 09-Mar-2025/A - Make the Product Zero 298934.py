2ffc8d14d99d7441105f75573f510840739b22ec
firaolgelana <gelanafiraol@gmail.com>
2025-03-09 22:12:53 +0300

    Created 'A - Make the Product Zero' by A2SV Hub

---
+++ A2SV G6 - Round #2 09-Mar-2025/A - Make the Product Zero 298934.py
@@ -0,0 +1,6 @@
+# Problem: A - Make the Product Zero - https://codeforces.com/gym/586960/problem/A
+
+n = int(input())
+arr = [abs(int(n)) for n in input().split()]
+min_num = min(arr)
+print(min_num)
