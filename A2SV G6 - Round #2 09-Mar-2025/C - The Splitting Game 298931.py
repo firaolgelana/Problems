4c4f362b4216140bc13d23b2ac57ba92a6e10888
firaolgelana <gelanafiraol@gmail.com>
2025-03-09 22:12:38 +0300

    Created 'C - The Splitting Game' by A2SV Hub

---
+++ A2SV G6 - Round #2 09-Mar-2025/C - The Splitting Game 298931.py
@@ -0,0 +1,16 @@
+# Problem: C - The Splitting Game - https://codeforces.com/gym/586960/problem/C
+
+from collections import Counter, defaultdict
+t = int(input())
+for i in range(t):
+    n = int(input())
+    s = input()
+    left, right = defaultdict(int), Counter(s)
+    count = 0
+    for i in range(n):
+        left[s[i]] += 1
+        right[s[i]] -= 1
+        if right[s[i]] == 0:
+            del right[s[i]]
+        count = max(count, len(left) + len(right))
+    print(count)
