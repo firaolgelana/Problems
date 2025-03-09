54062f72e0686738a63ec4edb5d3cf194e60df8d
firaolgelana <gelanafiraol@gmail.com>
2025-03-09 22:11:42 +0300

    Created 'F - Array Transformation' by A2SV Hub

---
+++ A2SV G6 - Round #2 09-Mar-2025/F - Array Transformation 298926.py
@@ -0,0 +1,15 @@
+# Problem: F - Array Transformation - https://codeforces.com/gym/586960/problem/F
+
+from collections import Counter
+t = int(input())
+for i in range(t):
+    n = int(input())
+    arr = [int(n) for n in input().split()]
+    count = Counter(arr)
+    freq = sorted(count.values(), reverse=True)
+    num = freq[0]
+    for i in range(1, len(freq)):
+        left = freq[i] * (i + 1)
+        num = max(left, num)
+    print(n - num)
+
