55c4bfbe83358cb3b88e396c7fcb23b35d3c5217
firaolgelana <gelanafiraol@gmail.com>
2025-03-12 12:35:58 +0300

    Created 'A - The Ticket Booth' by A2SV Hub

---
+++ A2SV G6 - Round #6 12-Mar-2025/A - The Ticket Booth 303691.py
@@ -0,0 +1,23 @@
+# Problem: A - The Ticket Booth - https://codeforces.com/gym/594077/problem/A
+
+from collections import defaultdict, deque, Counter
+import sys, math
+ 
+input = sys.stdin.readline
+ 
+def iin():return int(input())
+ 
+def sin():return input().strip()
+ 
+def lin():return list(map(int, input().split()))
+  
+def minp(): return map(int, input().split())
+
+def main():
+    n, s = minp()
+    print(math.ceil(s/n))
+
+if __name__ == "__main__":
+    t = 1
+    for i in range(t):
+        main()
