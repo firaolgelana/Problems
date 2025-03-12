0193ea82eef27a0e93d16cc9dafcbd295571bdbf
firaolgelana <gelanafiraol@gmail.com>
2025-03-12 12:37:05 +0300

    Created 'B - Nafargi's Binary Reduction Game' by A2SV Hub

---
+++ A2SV G6 - Round #6 12-Mar-2025/B - Nafargi's Binary Reduction Game 303697.py
@@ -0,0 +1,30 @@
+# Problem: B - Nafargi's Binary Reduction Game - https://codeforces.com/gym/594077/problem/B
+
+from collections import defaultdict, deque, Counter
+import sys, math
+ 
+input = sys.stdin.readline
+ 
+def iin():return int(input())
+ 
+def sin():return input().strip()
+ 
+def lin():return list(map(int, input().split()))
+  
+def minp(): return map(int, input().split())
+
+def main():
+    n = iin()
+    s = sin()
+    stack = []
+    for char in s:
+        if stack and stack[-1] != char:
+            stack.pop()
+        else:
+            stack.append(char)
+    print(len(stack))
+
+if __name__ == "__main__":
+    t = 1
+    for i in range(t):
+        main()
