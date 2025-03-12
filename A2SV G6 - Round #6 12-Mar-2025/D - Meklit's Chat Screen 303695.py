a4f9d92dde5c867dbc214d30d8b1ca07ff8b6358
firaolgelana <gelanafiraol@gmail.com>
2025-03-12 12:37:00 +0300

    Created 'D - Meklit's Chat Screen' by A2SV Hub

---
+++ A2SV G6 - Round #6 12-Mar-2025/D - Meklit's Chat Screen 303695.py
@@ -0,0 +1,39 @@
+# Problem: D - Meklit's Chat Screen - https://codeforces.com/gym/594077/problem/D
+
+from collections import defaultdict, deque, Counter
+import sys, math
+ 
+input = sys.stdin.readline
+ 
+def iin():return int(input())
+ 
+def sin():return input().strip()
+ 
+def lin():return list(map(int, input().split()))
+  
+def minp(): return map(int, input().split())
+
+def main():
+    n, m = minp()
+    chats = lin()
+    seen = set()
+    stack = deque()
+    for num in chats:
+        if num in seen:
+            continue
+        elif len(stack) < m:
+            stack.appendleft(num)
+            seen.add(num)
+        else:
+            seen.discard(stack.pop())
+            seen.add(num)
+            stack.appendleft(num)
+
+    print(len(stack))
+    print(*stack)
+
+
+if __name__ == "__main__":
+    t = 1
+    for i in range(t):
+        main()
