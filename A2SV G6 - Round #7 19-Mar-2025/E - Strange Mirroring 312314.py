ec474c3e7e157a0fecf37a522e644d99e51c7723
firaolgelana <gelanafiraol@gmail.com>
2025-03-19 14:51:43 +0300

    Created 'E - Strange Mirroring' by A2SV Hub

---
+++ A2SV G6 - Round #7 19-Mar-2025/E - Strange Mirroring 312314.py
@@ -0,0 +1,36 @@
+# Problem: E - Strange Mirroring - https://codeforces.com/gym/596141/problem/E
+
+from collections import defaultdict, deque, Counter
+from math import floor, ceil, log2
+import sys
+ 
+input = sys.stdin.readline
+ 
+def iin():return int(input())
+ 
+def sin():return input().strip()
+ 
+def lin():return list(map(int, input().split()))
+  
+def minp(): return map(int, input().split())
+ 
+def main():
+    s = sin()
+    n = iin()
+    queries = lin()
+    size = len(s)
+    def find(k):
+        if k <= size:
+            return s[k - 1]
+        operations = ceil(log2(k/size))
+        k -= ((2 ** (operations - 1)) * size)
+        return find(k).swapcase()
+ 
+    for query in queries:
+        print(find(query), end=" ")
+    print()
+ 
+if __name__ == "__main__":
+    t = iin()
+    for i in range(t):
+        main()
