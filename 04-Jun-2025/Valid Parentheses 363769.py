4a2efa661cbef97b386e6eeed907aa1c6f00521e
firaolgelana <gelanafiraol@gmail.com>
2025-06-04 23:00:51 +0300

    Created 'Valid Parentheses' by A2SV Hub

---
+++ 04-Jun-2025/Valid Parentheses 363769.py
@@ -0,0 +1,13 @@
+# Problem: Valid Parentheses - https://leetcode.com/problems/valid-parentheses
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        brackets = Counter({'(':')', '[':']', '{':'}'})
+        stack = []
+        for bracket in s:
+            if stack and brackets[stack[-1]] == bracket:
+                stack.pop()
+                continue
+            stack.append(bracket)
+
+        return not stack 
