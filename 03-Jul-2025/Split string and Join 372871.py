13ca5e836649542f563ad10bb039180db85c47fe
firaolgelana <gelanafiraol@gmail.com>
2025-07-03 10:06:31 +0300

    Created 'Split string and Join' by A2SV Hub

---
+++ 03-Jul-2025/Split string and Join 372871.py
@@ -0,0 +1,9 @@
+# Problem: Split string and Join - https://www.hackerrank.com/challenges/python-string-split-and-join/problem?isFullScreen=true
+
+def split_and_join(line):
+    return "-".join(line.split())
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
