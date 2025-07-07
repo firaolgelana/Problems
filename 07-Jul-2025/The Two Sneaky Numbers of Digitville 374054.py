21af5c6395d71ff9b60df8c3b91430be7c8da0c3
firaolgelana <gelanafiraol@gmail.com>
2025-07-07 13:58:57 +0300

    Created 'The Two Sneaky Numbers of Digitville' by A2SV Hub

---
+++ 07-Jul-2025/The Two Sneaky Numbers of Digitville 374054.py
@@ -0,0 +1,6 @@
+# Problem: The Two Sneaky Numbers of Digitville - https://leetcode.com/problems/the-two-sneaky-numbers-of-digitville/description
+
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        return [num for num, val in Counter(nums).items() if val >= 2]
+        
