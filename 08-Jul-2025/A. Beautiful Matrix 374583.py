044566c8861fe291e02f304bc38bf12e0786b719
firaolgelana <gelanafiraol@gmail.com>
2025-07-08 20:34:38 +0300

    Created 'A. Beautiful Matrix' by A2SV Hub

---
+++ 08-Jul-2025/A. Beautiful Matrix 374583.py
@@ -0,0 +1,9 @@
+# Problem: A. Beautiful Matrix - https://codeforces.com/problemset/problem/263/A
+
+matrix = [list(map(int, input().split())) for _ in range(5)]
+x, y = 0, 0
+for i in range(5):
+    for j in range(5):
+        if matrix[i][j]:
+            x, y = i + 1, j + 1
+print(abs(3 - x) + abs(y - 3))
