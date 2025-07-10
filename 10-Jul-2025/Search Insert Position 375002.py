91c66ea92c7be82ba10e0bff63b027bea7fd491a
firaolgelana <gelanafiraol@gmail.com>
2025-07-10 09:29:48 +0300

    Created 'Search Insert Position' by A2SV Hub

---
+++ 10-Jul-2025/Search Insert Position 375002.py
@@ -0,0 +1,6 @@
+# Problem: Search Insert Position - https://leetcode.com/problems/search-insert-position/
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        return bisect_left(nums, target)
+        
