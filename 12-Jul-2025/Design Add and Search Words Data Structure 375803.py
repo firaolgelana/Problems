775eb87d5dcfff6eaf7bfec98577b36e1f5c7dd5
firaolgelana <gelanafiraol@gmail.com>
2025-07-12 21:46:28 +0300

    Created 'Design Add and Search Words Data Structure' by A2SV Hub

---
+++ 12-Jul-2025/Design Add and Search Words Data Structure 375803.py
@@ -0,0 +1,36 @@
+# Problem: Design Add and Search Words Data Structure - https://leetcode.com/problems/design-add-and-search-words-data-structure/
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end = True
+
+    def search(self, word: str) -> bool:
+        def dfs(node, i):
+            if i == len(word):
+                return node.is_end
+            char = word[i]
+            if char == '.':
+                for child in node.children.values():
+                    if dfs(child, i + 1):
+                        return True
+                return False
+            else:
+                if char not in node.children:
+                    return False
+                return dfs(node.children[char], i + 1)
+
+        return dfs(self.root, 0)
