97aa1d67e09b71e3434eeb2e0593ac05a54856f1
firaolgelana <gelanafiraol@gmail.com>
2025-07-19 10:19:49 +0300

    Created 'X of a Kind in a Deck of Cards' by A2SV Hub

---
+++ 19-Jul-2025/X of a Kind in a Deck of Cards 377892.py
@@ -0,0 +1,5 @@
+# Problem: X of a Kind in a Deck of Cards - https://leetcode.com/problems/x-of-a-kind-in-a-deck-of-cards/
+
+class Solution:
+    def hasGroupsSizeX(self, deck: List[int]) -> bool:
+        return reduce(gcd, Counter(deck).values()) > 1
