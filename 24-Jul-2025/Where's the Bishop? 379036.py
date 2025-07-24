dc25afb8d30a23912b2ad566eb9c4569dc231cb6
firaolgelana <gelanafiraol@gmail.com>
2025-07-24 20:19:13 +0300

    Created 'Where's the Bishop?' by A2SV Hub

---
+++ 24-Jul-2025/Where's the Bishop? 379036.py
@@ -0,0 +1,15 @@
+# Problem: Where's the Bishop? - https://codeforces.com/problemset/problem/1692/C
+
+for _ in range(int(input())):
+    input()  
+    board = [input() for _ in range(8)]
+    
+    for r in range(1, 7):  
+        for c in range(1, 7):
+            if board[r][c] == '#' and \
+               board[r-1][c-1] == '#' and \
+               board[r-1][c+1] == '#' and \
+               board[r+1][c-1] == '#' and \
+               board[r+1][c+1] == '#':
+                print(r + 1, c + 1) 
+                break
